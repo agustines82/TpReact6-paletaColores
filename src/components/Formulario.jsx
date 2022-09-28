6cfd9b63768e0d03b9ad790254f131738c2dafee
Agustin Sampayo <agustines82@gmail.com>
2022-09-28 10:04:31 -0300

    maquetado del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -6,20 +6,23 @@ const Formulario = () => {
     return (
         <>
             <section className="container">
-                <Form className="container my-5">
+                <Form className="container my-5 bg-ligth">
                     <Form.Group className="mb-3 ms-1" controlId="formBasicText">
                         <Form.Label className="mt-2">Administrador de colores</Form.Label>
+                        <hr></hr>
                         <div className="row">
                             <aside className="col-2 ms-3 muestra"></aside>
                             <aside className="col-8 ms-3">
-                                <Form.Control type="text" placeholder="Ingresa un color" />
+                                <Form.Control size="sm" type="text" placeholder="Ingresa un color" />
                                 <Form.Text className="text-muted">Elige entre rojo, azul, verde o amarillo</Form.Text>
                             </aside>
                         </div>
+                        <div className="d-flex justify-content-center">
+                            <Button variant="dark" type="submit">
+                                Guardar
+                            </Button>
+                        </div>
                     </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
                 </Form>
             </section>
         </>
