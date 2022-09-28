50c50fa97281f2e53989993f5c6ad648b32641ed
Agustin Sampayo <agustines82@gmail.com>
2022-09-28 12:28:41 -0300

    creo el componente lista colores a los efectos de iterar el componente color. modifico maquetado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Formulario from "./components/Formulario";
-import Color from "./components/Color";
+
 function App() {
     return (
         <main>
             <Formulario />
-            <section className="container">
-                <aside className="container row justify-content-center align-items-center">
-                    <Color />
-                </aside>
-            </section>
         </main>
     );
 }
